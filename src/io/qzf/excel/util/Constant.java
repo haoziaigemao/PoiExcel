740faf9f8e3c3f128d98c97e790f8ff35b2ff1ea
qinfeo <18922619@qq.com>
2017-01-18 15:03:00 +0800

    导入和导出
    添加了主要工具类

---
+++ src/io/qzf/excel/util/Constant.java
@@ -0,0 +1,15 @@
+package io.qzf.excel.util;
+/**
+ * 导入卡需要的字段与中文对应常量
+ * @author Jwenk
+ *
+ */
+public class Constant {
+	//居民健康卡
+	public static String  RHCARD ="card_status=卡状态,id=id,seq=序号,name=姓名,sex=性别,nation=民族,birth_date=出生日期,id_card=公民身份号码,addr=地址,house_holder=户主,relation=与户主关系,link_phone=联系电话,photo=公安照片,activation_marker=激活标志,modify_time=修改时间,mark=备注,lkr=领卡人,lkrsfzh=领卡人身份证号,ffbz=发放标志,fkbz=发卡标志";
+	//新农合卡
+	public static String  XNHCARD ="status0=卡状态,chzh000=参合证号,xzqdm00=行政区代码,xzqm000=行政区名,chryh00=参合人员号,chrym00=参合人员名,sex0000=性别,ICcard0=IC卡号,bnjf000=本年缴费,jzfprq0=精准扶贫人群,yjf0000=预交费,csrq000=出生日期,chrq000=参合日期,IDcard0=身份证号,sfhz000=是否户主,nation0=民族,yhzgx00=与户主关系,address=家庭住址,phone00=联系电话,wgdw000=务工单位,wgdz000=务工地址,zzch000=终止参合,chxzdm0=参合性质代码,remark0=备注,mxbzh00=慢性病证号,mxbns00=慢性病年审,sfhmd00=是否黑名单,hmdyybz=黑名单原因备注,lkr0000=领卡人,lkrsfzh=领卡人身份证,ffbz000=发放标志,fkbz000=发卡标志";
+	//社会保障卡
+	public static String  SINCARD = "shbzkkh=社会保障卡卡号,fkjhxh0=发卡计划序号,kpch000=卡批次编号,sbh0000=社保号,xm00000=姓名,sfzh000=身份证号,dwmc000=单位名称,klb0000=卡类别,ksbm000=卡识别码,cshjgbm=初始化话机构编码,zkzt000=制卡主体,zkcgbz0=制卡成功标志,fkbz000=发卡标志,ffbz000=发放标志,ffsj000=发放时间,fankasj=返卡时间,zkr0000=制卡人,hkr0000=回卡人,hksj000=回卡时间,fakaren=发卡人,fakasj0=发卡时间,lkrxm00=领卡人,lkrsfzh=领卡人身份证号,yhmc000=银行名称,jbr0000=经办人,sbjbjg0=社保经办机构,jbsj000=经办时间,kzt0000=卡状态";
+}
+
